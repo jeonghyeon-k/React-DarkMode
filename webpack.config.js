8eaa4174273a984a37f4aa0d7b7db0fe22152132
jeonghyeon kim <rlawjd147@gmail.com>
2021-09-01 17:46:14 +0900

    webpack과 Babel을 이용한 React 개발 환경

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const port = process.env.PORT || 3000;
+module.exports = {
+  mode: process.env.NODE_ENV || "development", // 1
+  entry: "./src/index.js", // 2
+  output: {
+    // 3
+    filename: "bundle.[hash].js", // 4
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html',
+    })
+  ],
+  module: {
+    rules: [
+      {
+        // 1
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        // 2
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: {
+              minimize: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  devServer:{
+    host: 'localhost',
+    port: port,
+    historyApiFallback: true
+  }
+};
